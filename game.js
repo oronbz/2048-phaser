24bdd083ee7a9dd277b16878b8efeed553084146
Oron Ben Zvi <oronbz@gmail.com>
2018-09-05 00:25:21 +0300

    generate 2 tiles

--- game.js
+++ game.js
@@ -48,19 +48,49 @@ class PlayGame extends Phaser.Scene {
     }
 
     create() {
+        this.boardArray = [];
         for (var i=0; i<gameOptions.boardSize.rows; i++) {
+            this.boardArray[i] = [];
             for (var j=0; j<gameOptions.boardSize.cols; j++) {
                 var tilePosition = this.getTilePosition(i, j);
                 this.add.image(tilePosition.x, tilePosition.y, "emptytile");
-                this.add.sprite(tilePosition.x, tilePosition.y, "tiles", 5);
+                var tile = this.add.sprite(tilePosition.x, tilePosition.y, "tiles", 0);
+                tile.visible = false;
+                this.boardArray[i][j] = {
+                    tileValue: 0,
+                    tileSprite: tile
+                }
             }
         }
+        this.addTile();
+        this.addTile();
     }
 
     getTilePosition(row, col) {
         var posx = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize * (col + 0.5);
         var posy = gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize * (row + 0.5);
         return new Phaser.Geom.Point(posx, posy);
+    }
+    
+    addTile() {
+        var emptyTiles = []
+        for (var i=0; i<gameOptions.boardSize.rows; i++) {
+            for (var j=0; j<gameOptions.boardSize.cols; j++) {
+                if (this.boardArray[i][j].tileValue == 0) {
+                    emptyTiles.push({
+                        row: i,
+                        col: j
+                    })
+                }
+            }
+        }
+
+        if (emptyTiles.length > 0) {
+            var chosenTile = Phaser.Utils.Array.GetRandom(emptyTiles);
+            this.boardArray[chosenTile.row][chosenTile.col].tileValue = 1;
+            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.visible = true;
+            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.setFrame(0);
+        }
     }
 }
 
