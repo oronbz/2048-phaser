0b11aaa4304ebb1e1e11faed4f30e7afe8d41004
Oron Ben Zvi <oronbz@gmail.com>
2018-09-04 23:28:57 +0300

    Add BootGame scene

--- game.js
+++ game.js
@@ -4,13 +4,34 @@ window.onload = function() {
         width: 480,
         height: 640,
         backgroundColor: 0xff0000,
-        scene: PlayGame
+        scene: [BootGame, PlayGame]
     }
 
     game = new Phaser.Game(gameConfig);
     window.focus();
     resizeGame();
     window.addEventListener("resize", resizeGame);
+}
+
+class BootGame extends Phaser.Scene {
+    constructor() {
+        super("BootGame");
+    }
+
+    create() {
+        console.log("game is booting...");
+        this.scene.start("PlayGame");
+    }
+}
+
+class PlayGame extends Phaser.Scene {
+    constructor() {
+        super("PlayGame");
+    }
+
+    create() {
+        console.log("this is my awesome game");
+    }
 }
 
 function resizeGame() {
@@ -25,15 +46,5 @@ function resizeGame() {
     } else {
         canvas.style.width = (windowHeight * gameRatio) + "px";
         canvas.style.height = windowHeight + "px";
-    }
-}
-
-class PlayGame extends Phaser.Scene {
-    constructor() {
-        super("PlayGame");
-    }
-
-    create() {
-        console.log("this is my awesome game");
     }
 }
